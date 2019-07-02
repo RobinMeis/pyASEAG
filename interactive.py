f58eed3be12225466d37746bfd6b047a0a64d699
RobinMeis <robin.meis@smartnoob.de>
2019-07-02 23:10:29 +0200

    Interactive Console to find your connection

---
+++ interactive.py
@@ -0,0 +1,64 @@
+from pyASEAG.vehicles import vehicles
+from pyASEAG.stopPoints import stopPoints
+from pyASEAG.journey import journey
+
+import code
+import readline
+import rlcompleter
+import dill
+import sys
+
+def help():
+    print("TODO: help to implement help()")
+
+def save(filename="lastsession.pkl"):
+    dill.dump_session(filename)
+
+def restore(filename="lastsession.pkl"):
+    dill.load_session(filename)
+
+def fetcher(stops):
+    while True:
+        print("Fetching")
+        try:
+            stops.fetchDepartures()
+        except:
+            print("Failed")
+            time.sleep(10)
+        else:
+            print("Done")
+            time.sleep(60)
+
+busses = vehicles()
+stops = stopPoints(busses)
+stops.fetch()
+bushof = stops.printFind("Aachen Bushof")
+talbot = stops.printFind("Talbot")
+hansemann = stops.printFind("Hansemannplatz")
+ponttor = stops.printFind("Ponttor")
+viktoriaallee = stops.printFind("Viktoriaallee")
+elisenbrunnen = stops.printFind("Elisenbrunnen")
+normaluhr = stops.printFind("Normaluhr")
+ludwig = stops.printFind("Ludwig Forum")
+stawag = stops.printFind("STAWAG")
+bushof.enableGlobalFetching()
+ponttor.enableGlobalFetching()
+hansemann.enableGlobalFetching()
+talbot.enableGlobalFetching()
+normaluhr.enableGlobalFetching()
+ludwig.enableGlobalFetching()
+elisenbrunnen.enableGlobalFetching()
+viktoriaallee.enableGlobalFetching()
+stawag.enableGlobalFetching()
+stops.fetchDepartures()
+
+vars = globals()
+vars.update(locals())
+readline.set_completer(rlcompleter.Completer(vars).complete)
+readline.parse_and_bind("tab: complete")
+shell = code.InteractiveConsole(vars)
+shell.interact(
+  banner="Welcome to ASEAG Interactive Shell\n\nType restore() to restore a session or help() for more info\n",
+  exitmsg="\nASEAG Interactive Vehicle Shell has been terminated. Your session has been automatically stored in lastsession.pkl"
+)
+save()
