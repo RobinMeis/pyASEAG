59917e82a663f9fb5660848f99f48f811b29909c
RobinMeis <robin.meis@smartnoob.de>
2019-07-02 23:04:01 +0200

    Updated

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ with open("README.md", "r") as fh:
 setuptools.setup(
      name='pyASEAG',
      version='0.1',
-     scripts=['pyASEAG'] ,
      author="Robin Meis",
      author_email="blog@smartnoob.de",
      description="A Python3 Library and interactive Shell to access FormelASEAGs race data",
