2ff2039ddca42b5b0b3dddb5d88555678811888f
RobinMeis <robin.meis@smartnoob.de>
2019-07-16 17:20:18 +0200

    Avoid adding a same but different departure objects to a vehicle multiple times

--- pyASEAG/departuresVehicle.py
+++ pyASEAG/departuresVehicle.py
@@ -1,17 +1,20 @@
 class departuresVehicle:
     def __init__(self, vehicle):
         self.vehicle = vehicle
-        self.departures = []
+        self.departures = {}
 
     def addDeparture(self, newDeparture):
-        for departure in self.departures:
-            if (departure == newDeparture):
-                return
-
-        self.departures.append(newDeparture)
+        try:
+            self.departures[newDeparture.tripId]
+        except:
+            self.departures[newDeparture.tripId] = newDeparture
+            return newDeparture
+        else:
+            self.departures[newDeparture.tripId].update(newDeparture)
+            return self.departures[newDeparture.tripId]
 
     def getDepartures(self):
-        departures = sorted(self.departures, key = lambda i: i.estimatedTime)
+        departures = sorted(self.departures.values(), key = lambda i: i.estimatedTime)
         return departures
 
     def printDepartures(self):
